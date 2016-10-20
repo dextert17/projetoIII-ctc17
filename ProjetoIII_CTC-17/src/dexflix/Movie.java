5bf49cbb5ce26d82c90af494340c2c4308b2cdd6
Mattheus Jucá <Mattheus Juc>
2016-10-20 16:14:21 -0200

    Leitura do arquivo de filmes

--- ProjetoIII_CTC-17/src/dexflix/Movie.java
+++ ProjetoIII_CTC-17/src/dexflix/Movie.java
@@ -1,5 +1,37 @@
 package dexflix;
 
-public class Movie {
+import java.util.ArrayList;
 
+public class Movie {
+	
+	private int id;
+	private String title;
+	private ArrayList<String> genres;
+	
+	public Movie (int id, String title) {
+		this.id = id;
+		this.title = title;
+		genres = new ArrayList<String>();
+	}
+	
+	public void addGenre (String genre) {
+		this.genres.add(genre);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public ArrayList<String> getGenres() {
+		return genres;
+	}
+	
+	public void setGenres(ArrayList<String> genres) {
+		this.genres = genres;
+	}
+	
 }
