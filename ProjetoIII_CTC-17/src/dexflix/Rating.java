e8ecd684ad621437f919b9486d4e1a61f5b358a8
Mattheus Jucá <Mattheus Juc>
2016-10-24 09:43:11 -0200

    Leitura de todos os arquivos

--- ProjetoIII_CTC-17/src/dexflix/Rating.java
+++ ProjetoIII_CTC-17/src/dexflix/Rating.java
@@ -1,5 +1,33 @@
 package dexflix;
 
 public class Rating {
+	
+	private int userId;
+	private int movieId;
+	private int rating;
+	private int timestamp;
+	
+	public Rating (int userId, int movieId, int rating, int timestamp) {
+		this.userId = userId;
+		this.movieId = movieId;
+		this.rating = rating;
+		this.timestamp = timestamp;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public int getMovieId() {
+		return movieId;
+	}
 
+	public int getRating() {
+		return rating;
+	}
+
+	public int getTimestamp() {
+		return timestamp;
+	}
+	
 }
